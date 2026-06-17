8cdb3c029bc72f1bade99fe945bb72c511cada4a
Rahul Jain <rahuljait@gmail.com>
2026-06-17 20:48:31 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+// const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+// Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+module.exports = new Sequelize('codegig', 'postgres', '1234', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
